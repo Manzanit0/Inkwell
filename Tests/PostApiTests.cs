1dd7e1156da408768855a4d0de34df75f70b25dd
Manzanit0 <unico.javier@gmail.com>
2018-11-17 21:25:21 +0000

    Cleaned up API tests.

--- Tests/PostApiTests.cs
+++ Tests/PostApiTests.cs
@@ -1,137 +1,125 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Xunit;
-
 using API.Posts;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Xunit;
 
 namespace Tests
 {
-    public class PostApiTests
+    public class PostApiTests : IDisposable
     {
-        [Fact]
-        public void GetReturnsAllRecords()
+        public PostApiTests()
         {
-            var dbContext = CreateDbContextWithData();
-            var controller = new PostsController(dbContext);
-
-            var result = controller.Get() as OkObjectResult;
-            var posts = result.Value as List<Post>;
-            
-            Assert.Equal(2, posts.Count);
-        }
-        
-        [Fact]
-        public void GetWithIdReturnsCorrectRecord()
-        {
-            var dbContext = CreateDbContextWithData();
-            var controller = new PostsController(dbContext);
-            var posts = dbContext.Posts.ToList();
-            
-            var result = controller.Get(posts[0].Id) as OkObjectResult;
-            var post = result.Value as Post;
-            
-            Assert.Equal(posts[0].Id, post.Id);
-        }
-        
-        [Fact]
-        public void PostCreatesNewRecord()
-        {
-            var dbContext = CreateDbContextWithData();
-            Assert.Equal(2, dbContext.Posts.ToList().Count);
-            
-            var controller = new PostsController(dbContext);
-                       
-            var result = controller.Post(new Post()) as CreatedResult;
-            var post = result.Value as Post;
-
-            var posts = dbContext.Posts.ToList();
-            Assert.Equal(3, posts.Count);
-            Assert.Equal(1, posts.FindAll(x => x.Id == post.Id).Count);
-            Assert.Equal($"/posts/{post.Id}", result.Location);
-        }
-        
-        [Fact]
-        public void PutCreatesNewRecord()
-        {
-            var dbContext = CreateDbContextWithData();
-            Assert.Equal(2, dbContext.Posts.ToList().Count);
-            
-            var controller = new PostsController(dbContext);
-            
-            var result = controller.Put(Guid.NewGuid(), new Post()) as CreatedResult;
-            var post = result.Value as Post;
-
-            var posts = dbContext.Posts.ToList();           
-            Assert.Equal(3, posts.Count);
-            Assert.Equal(1, posts.FindAll(x => x.Id == post.Id).Count);
-            Assert.Equal($"/posts/{post.Id}", result.Location);
-        }
-        
-        [Fact]
-        public void PutOverridesExistingRecord()
-        {
-            var dbContext = CreateDbContextWithData();
-            Assert.Equal(2, dbContext.Posts.ToList().Count);                        
-
-            var existingPost = dbContext.Posts.FirstOrDefault();
-            existingPost.Title = "Changed Title";
-            
-            var controller = new PostsController(dbContext);
-            var result = controller.Put(existingPost.Id, existingPost) as OkObjectResult;
-            var post = result.Value as Post;
-
-            var posts = dbContext.Posts.ToList();           
-            Assert.Equal(2, posts.Count);         
-            Assert.Equal("Changed Title", posts.Find(x => x.Id == post.Id).Title);
+            _dbContext = CreateDbContextWithData();
+            _controller = new PostsController(_dbContext);
         }
 
-        [Fact]
-        public void PutReturnsBadRequestUponIdMismatch()
+        public void Dispose()
         {
-            var dbContext = CreateDbContextWithData();            
-            var controller = new PostsController(dbContext);
-            var result = controller.Put(Guid.NewGuid(), new Post {Id = Guid.NewGuid() }) as BadRequestObjectResult;
-
-            Assert.NotNull(result);
-            
-            var posts = dbContext.Posts.ToList();           
-            Assert.Equal(2, posts.Count);
+            _dbContext.Dispose();
         }
-        
-        [Fact]
-        public void DeletePost()
-        {
-            var dbContext = CreateDbContextWithData();
-            Assert.Equal(2, dbContext.Posts.ToList().Count);                        
 
-            var existingPost = dbContext.Posts.FirstOrDefault();
+        private readonly PostsDbContext _dbContext;
+        private readonly PostsController _controller;
 
-            var controller = new PostsController(dbContext);
-            var result = controller.Delete(existingPost.Id) as OkResult;
-            
-            Assert.NotNull(result);
+        private List<Post> DbPosts => _dbContext.Posts.ToList();
 
-            var posts = dbContext.Posts.ToList();           
-            Assert.Equal(1, posts.Count);         
-            Assert.Null(posts.Find(x => x.Id == existingPost.Id));
-        }
-        
         private static PostsDbContext CreateDbContextWithData()
         {
             var dbContext = new PostsDbContext(Options);
-            dbContext.Posts.Add(new Post { Id = Guid.NewGuid(), Author = "XXX", Title = "YYY", Content = "ZZZ" });
-            dbContext.Posts.Add(new Post { Id = Guid.NewGuid(), Author = "XXX", Title = "YYY", Content = "ZZZ" });
-            dbContext.SaveChanges();            
+            dbContext.Posts.Add(new Post {Id = Guid.NewGuid(), Author = "XXX", Title = "YYY", Content = "ZZZ"});
+            dbContext.Posts.Add(new Post {Id = Guid.NewGuid(), Author = "XXX", Title = "YYY", Content = "ZZZ"});
+            dbContext.SaveChanges();
             return dbContext;
         }
 
         private static DbContextOptions<PostsDbContext> Options =>
             new DbContextOptionsBuilder<PostsDbContext>()
                 .UseInMemoryDatabase(Guid.NewGuid().ToString())
-                .EnableSensitiveDataLogging()                
+                .EnableSensitiveDataLogging()
                 .Options;
+
+        [Fact]
+        public void DeletePost()
+        {
+            Assert.Equal(2, DbPosts.Count);
+
+            var existingPost = DbPosts.FirstOrDefault();
+            var result = _controller.Delete(existingPost.Id) as OkResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(1, DbPosts.Count);
+            Assert.Null(DbPosts.Find(x => x.Id == existingPost.Id));
+        }
+
+        [Fact]
+        public void GetReturnsAllRecords()
+        {
+            var result = _controller.Get() as OkObjectResult;
+            var posts = result.Value as List<Post>;
+
+            Assert.Equal(2, posts.Count);
+        }
+
+        [Fact]
+        public void GetWithIdReturnsCorrectRecord()
+        {
+            var result = _controller.Get(DbPosts[0].Id) as OkObjectResult;
+            var post = result.Value as Post;
+
+            Assert.Equal(DbPosts[0].Id, post.Id);
+        }
+
+        [Fact]
+        public void PostCreatesNewRecord()
+        {
+            Assert.Equal(2, DbPosts.Count);
+
+            var result = _controller.Post(new Post()) as CreatedResult;
+            var post = result.Value as Post;
+
+            Assert.Equal(3, DbPosts.Count);
+            Assert.Equal(1, DbPosts.FindAll(x => x.Id == post.Id).Count);
+            Assert.Equal($"/posts/{post.Id}", result.Location);
+        }
+
+        [Fact]
+        public void PutCreatesNewRecord()
+        {
+            Assert.Equal(2, DbPosts.Count);
+
+            var result = _controller.Put(Guid.NewGuid(), new Post()) as CreatedResult;
+            var post = result.Value as Post;
+
+            Assert.Equal(3, DbPosts.Count);
+            Assert.Equal(1, DbPosts.FindAll(x => x.Id == post.Id).Count);
+            Assert.Equal($"/posts/{post.Id}", result.Location);
+        }
+
+        [Fact]
+        public void PutOverridesExistingRecord()
+        {
+            Assert.Equal(2, DbPosts.Count);
+
+            var existingPost = DbPosts.FirstOrDefault();
+            existingPost.Title = "Changed Title";
+
+            var result = _controller.Put(existingPost.Id, existingPost) as OkObjectResult;
+            var post = result.Value as Post;
+
+            Assert.Equal(2, DbPosts.Count);
+            Assert.Equal("Changed Title", DbPosts.Find(x => x.Id == post.Id).Title);
+        }
+
+        [Fact]
+        public void PutReturnsBadRequestUponIdMismatch()
+        {
+            var result = _controller.Put(Guid.NewGuid(), new Post {Id = Guid.NewGuid()}) as BadRequestObjectResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(2, DbPosts.Count);
+        }
     }
 }
