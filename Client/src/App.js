9f63ef0c840815a6573d6b6ab4d24a02da848104
Manzanit0 <unico.javier@gmail.com>
2018-11-26 00:21:32 +0000

    Wires up the Client to seed from the backend via the API.

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,23 +3,30 @@ import { PostList } from './components/PostList/PostList';
 import { Navbar } from './components/Navbar/Navbar';
 
 class App extends Component {
-  render() {
-    const posts = [
-      { title: "First post", blurb: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque non nunc ac nibh dictum mattis nec ut mauris..." },
-      { title: "Another post", blurb: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque non nunc ac nibh dictum mattis nec ut mauris..." },
-      { title: "First post", blurb: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque non nunc ac nibh dictum mattis nec ut mauris..." },
-      { title: "Another post", blurb: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque non nunc ac nibh dictum mattis nec ut mauris..." },
-      { title: "First post", blurb: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque non nunc ac nibh dictum mattis nec ut mauris..." },
-      { title: "Another post", blurb: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque non nunc ac nibh dictum mattis nec ut mauris..." },
-      { title: "First post", blurb: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque non nunc ac nibh dictum mattis nec ut mauris..." },
-      { title: "Another post", blurb: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque non nunc ac nibh dictum mattis nec ut mauris..." },
-      { title: "Yet another post", blurb: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque non nunc ac nibh dictum mattis nec ut mauris..." }
-    ]
+  constructor() {
+    super();
 
+    this.state = { posts: [] };
+
+    fetch('https://localhost:5001/api/posts') // C# API.
+      .then(response => {
+        return response.json();
+      })
+      .then(myJson => {
+        const posts = myJson.map(post => {
+          post.blurb = post.content.substring(0, 120) + "...";
+          return post;
+        });
+
+        this.setState({posts: posts});
+      });
+  }
+
+  render() {
     return (
       <div>
         <Navbar />
-        <PostList posts={posts} />
+        <PostList posts={this.state.posts} />
       </div>
     );
   }
