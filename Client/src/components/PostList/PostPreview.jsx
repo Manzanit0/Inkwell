c1290963cf93d8490a4bc1f64fc37138f531e18a
Manzanit0 <unico.javier@gmail.com>
2019-04-26 17:22:54 +0100

    Deletes dead code in PostPreview.

--- Client/src/components/PostList/PostPreview.jsx
+++ Client/src/components/PostList/PostPreview.jsx
@@ -1,22 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { PostView } from '../PostView/PostView'
 import './PostPreview.css'
 
 export class PostPreview extends Component {
     render() {
         return (
             <Link to={{pathname: `/post/${this.props.title}`}} style={{ textDecoration: 'none', color: 'black'}}>
-                <div className="post-preview-card" onClick={() => this.onClick(this.props)}>
+                <div className="post-preview-card">
                     <h3 className="post-preview-card__heading">{this.props.title}</h3>
                     <p>{this.props.blurb}</p>
                 </div>
             </Link>
         );
     }
-
-    onClick = (post) => {
-        //alert(JSON.stringify(post.title));
-        // TODO Route to single post view.
-    };
 }
